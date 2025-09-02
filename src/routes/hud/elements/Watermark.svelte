27415f768e6068744c3da11f23cd3107493b0ed9
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2025-09-02 10:10:17 +0200

    feat: wurst watermark

--- src/routes/hud/elements/Watermark.svelte
+++ src/routes/hud/elements/Watermark.svelte
@@ -1,10 +1,42 @@
-<img class="watermark" src="img/lb-logo.svg" alt="watermark" />
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { getSelectedProtocol } from "../../../integration/rest";
+
+    let gameVersion = "1.21.4";
+    $: versionString = `v7.41.1 MC ${gameVersion}`;
+
+    onMount(async () => {
+        const protocol = await getSelectedProtocol();
+        gameVersion = protocol.name;
+    });
+</script>
+
+<div class="watermark">
+    <img class="logo" src="img/wurst_128.png" alt="logo" />
+    <div class="version">{versionString}</div>
+</div>
 
 <style lang="scss">
     .watermark {
-        //position: fixed;
-        //top: 15px;
-        //left: 15px;
-        width: 165px;
+        display: flex;
+        align-items: center;
+        column-gap: 10px;
+        font-size: 20px;
+        padding-right: 5px;
+        background-color: rgba(white, 0.5);
+        height: 22px;
+
+        position: fixed;
+        left: 0;
+        top: 12px;
+
+        .logo {
+            height: 35px;
+        }
+
+        .version {
+            color: black;
+            white-space: nowrap;
+        }
     }
 </style>
