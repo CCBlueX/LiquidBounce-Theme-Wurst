c6e17d5c13f8d30c07ed981b1949fd93efbc3261
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2025-09-02 10:10:24 +0200

    feat: wurst arraylist

--- src/routes/hud/elements/ArrayList.svelte
+++ src/routes/hud/elements/ArrayList.svelte
@@ -1,39 +1,17 @@
 <script lang="ts">
-    import {onMount, tick} from "svelte";
-    import type {Module} from "../../../integration/types";
-    import {getModules} from "../../../integration/rest";
-    import {listen} from "../../../integration/ws";
-    import {getTextWidth} from "../../../integration/text_measurement";
-    import {flip} from "svelte/animate";
-    import {fly} from "svelte/transition";
-    import {convertToSpacedString, spaceSeperatedNames} from "../../../theme/theme_config";
+    import { onMount } from "svelte";
+    import { fly } from "svelte/transition";
+  import { getModules } from "../../../integration/rest";
+  import { Module } from "../../../integration/types";
+  import { listen } from "../../../integration/ws";
 
     let enabledModules: Module[] = [];
 
     async function updateEnabledModules() {
-        const modules = await getModules();
-        const visibleModules = modules.filter(m => m.enabled && !m.hidden);
-
-        const modulesWithWidths = visibleModules.map(module => {
-                let formattedName = $spaceSeperatedNames ? convertToSpacedString(module.name) : module.name;
-                let fullName = module.tag == null ? formattedName : formattedName + " " + module.tag;
-
-                return {
-                    ...module,
-                    width: getTextWidth(fullName, "500 14px Inter")
-                };
-            }
-        );
-
-        modulesWithWidths.sort((a, b) => b.width - a.width);
-
-        enabledModules = modulesWithWidths;
-        await tick();
+        enabledModules = (await getModules())
+            .filter((m) => m.enabled && !m.hidden)
+            .sort((a, b) => a.name.localeCompare(b.name));
     }
-
-    spaceSeperatedNames.subscribe(async () => {
-        await updateEnabledModules();
-    });
 
     onMount(async () => {
         await updateEnabledModules();
@@ -49,32 +27,26 @@
 </script>
 
 <div class="arraylist">
-    {#each enabledModules as {name, tag} (name)}
-        <div class="module" animate:flip={{ duration: 200 }} transition:fly={{ x: 50, duration: 200 }}>
-            {$spaceSeperatedNames ? convertToSpacedString(name) : name}
-            {#if tag}
-                <span class="tag"> {tag}</span>
-            {/if}
+    {#each enabledModules as { name } (name)}
+        <div class="module" in:fly={{ x: -100, duration: 300 }} out:fly={{ x: -100, duration: 300 }}>
+            {name}
         </div>
     {/each}
 </div>
 
 <style lang="scss">
-  @use "../../../colors.scss" as *;
+    .arraylist {
+        margin-left: 5px;
 
-  .module {
-    background-color: rgba($arraylist-base-color, 0.68);
-    color: $arraylist-text-color;
-    font-size: 14px;
-    border-radius: 4px 0 0 4px;
-    padding: 5px 8px;
-    border-left: solid 4px $accent-color;
-    width: max-content;
-    font-weight: 500;
-    margin-left: auto;
-  }
+        position: fixed;
+        left: 0;
+        top: 40px;
 
-  .tag {
-    color: $arraylist-tag-color;
-  }
+        .module {
+            font-size: 20px;
+            line-height: 20px;
+            color: white;
+            text-shadow: black 2px 2px;
+        }
+    }
 </style>
